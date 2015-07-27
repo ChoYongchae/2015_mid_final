0d477fabaa69de90070506a2f75984e3a8de11b0
Choyongchae <kbjb7535@naver.com>
2015-07-27 17:28:00 +0900

    test4

--- app/src/main/java/com/example/cyc/board/MainActivity.java
+++ app/src/main/java/com/example/cyc/board/MainActivity.java
@@ -4,14 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private String[] boardArray = { "1st", "2nd", "3rd", "4rd", "5", "6", "7" };
+    private ListView boardListView;
+    private ArrayAdapter arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boardListView = (ListView) findViewById(R.id.boardListView);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, boardArray);
+        boardListView.setAdapter(arrayAdapter);
     }
 
     @Override
